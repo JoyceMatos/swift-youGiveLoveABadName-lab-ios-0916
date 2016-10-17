b0ae5da88b20e96b5ee00d9bc921137d56126bef
JoyceMatos <j.matosjoyce@gmail.com>
2016-10-17 13:09:20 -0400

    Lab Completed

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var bonJovi: UIImageView!
+    
+    var buttonTapped = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,9 +19,67 @@ class ViewController: UIViewController {
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        
+        switch buttonTapped {
+        case false:
+        increaseSize()
+            print(buttonTapped)
+            buttonTapped = true
+        case true:
+            print(buttonTapped)
+            returnToOriginalSize()
+            buttonTapped = false
+        }
 
     }
+    
 
+    func increaseSize() {
+        
+     UIView.animateKeyframes(withDuration: 0.7, delay: 0.0, options: .calculationModeLinear, animations: {
+        
+        UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.3, animations: {
+            self.bonJovi.bounds.size.height = self.view.frame.size.height + 200
+        })
+        
+        UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.2, animations: {
+            self.bonJovi.bounds.size.height = self.view.frame.size.height
+        })
+        
+        UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.0, animations: {
+            self.bonJovi.bounds.size.height = self.view.frame.size.height
+        })
+        
+        self.view.layoutIfNeeded()
+        
+        }, completion: nil)
+        
+        
+        
+    }
+    
+    func returnToOriginalSize() {
+        
+        UIView.animateKeyframes(withDuration: 0.8, delay: 0.0, options: .calculationModeLinear, animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.3, animations: {
+                self.bonJovi.bounds.size.height = self.bonJovi.bounds.size.height/3 - 100
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.2, animations: {
+                self.bonJovi.bounds.size.height += 150
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.0, animations: {
+                self.bonJovi.bounds.size.height = self.bonJovi.bounds.size.height
+            })
+            
+            self.view.layoutIfNeeded()
+            
+            }, completion: nil)
+        
+    }
+    
+    
+    
 }
 
